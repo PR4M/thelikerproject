6136c9fea5dd06480686737ab52784d71086bd2b
Pramana <pramanadiputra@gmail.com>
2019-09-10 10:19:35 +0800

    Setup Laravel & Vue

---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="container">
+        It works
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
