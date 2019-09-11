eff93353237c4867737e4fe14ecc5fc1d0951be1
Pramana <pramanadiputra@gmail.com>
2019-09-11 10:43:53 +0800

    Laravel Fractal, Transformer for Post and User

---
+++ app/Transformers/PostTransformer.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Transformers;
+
+use App\Post;
+use App\Transformers\UserTransformer;
+use League\Fractal\TransformerAbstract;
+
+class PostTransformer extends TransformerAbstract
+{
+
+    protected $defaultIncludes = [
+        'author'
+    ];
+
+    /**
+     * A Fractal transformer.
+     *
+     * @return array
+     */
+    public function transform(Post $post)
+    {
+        return [
+            'id' => $post->id
+        ];
+    }
+
+    public function includeAuthor(Post $post)
+    {
+        return $this->item($post->user, new UserTransformer());
+    }
+}
